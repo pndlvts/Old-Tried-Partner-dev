51cd3f5011059ee728f9fbd1466c114b113ed5be
Pandelov Timofey <pndlvts@gmail.com>
2020-03-11 21:12:08 +0300

    header готов

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,128 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <header class="header">
+            <div class="header__wrapper">
+                <div class="header__container">
+                    <div class="header__row">
+                        <nav class="main-navigation">
+                            <div class="main-navigation__logo">
+                                <router-link to="/"><img src="./assets/media/logo.svg" title="We are Old Tried Partner" alt="logo"></router-link>
+                            </div>
+                            <div class="main-navigation__item">
+                                <router-link to="/products">Продукция</router-link>
+                            </div>
+                            <div class="main-navigation__item">
+                                <router-link to="/documents">Документы</router-link>
+                            </div>
+                            <div class="main-navigation__item">
+                                <router-link to="/contacts">Контакты</router-link>
+                            </div>
+                            <div class="main-navigation__item">
+                                <router-link to="/about">О нас</router-link>
+                            </div>
+                        </nav>
+                        <div class="phones">
+                            <ul class="phones__list">
+                                <li class="phones__item">
+                                    <a class="phones__phone" href="tel:89952005218">+7 (995) 200-52-18</a>
+                                </li>
+                                <li class="phones__item">
+                                    <a class="phones__phone" href="tel:89952005218">+7 (908) 692-32-18</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss">
+    @import "assets/styles/main.scss";
 
-#nav {
-  padding: 30px;
-}
+    #app {
+        font-family: 'PT Sans';
+        text-align: center;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    .header {
+        background: $company;
+        width: 100%;
+        position: fixed;
+        &__container {
+            @include container();
+        }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+        &__row {
+            @include row-flex();
+            display: flex;
+            justify-content: space-between;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+    }
+    .main-navigation{
+        display: flex;
+        align-items: center;
+        &__item {
+            display: inline-block;
+        }
+        &__item a,
+        a:visited,
+        a:hover,
+        a:active {
+            font-weight: bold;
+            color: $white;
+        }
+        &__item a{
+            padding: 10px;
+        }
+        &__item a.router-link-exact-active {
+            color: $white;
+            position: relative;
+        }
+        &__item a.router-link-exact-active::before {
+            content: "";
+            display: block;
+            width: 100%;
+            height: 1px;
+            position: absolute;
+            left: 0;
+            top: 0;
+            background: $white;
+        }
+
+        &__item a.router-link-exact-active::after {
+            content: "";
+            display: block;
+            width: 100%;
+            height: 1px;
+            position: absolute;
+            left: 0;
+            top: 100%;
+            background: $white;
+        }
+        &__logo{
+            img {
+                width: 50px;
+                height: 50px;
+            }
+        }
+    }
+
+    .phones {
+        display: flex;
+        align-items: center;
+        &__phone, &__phone:visited, &__phone:hover, &__phone:active {
+            color: $white;
+            font-size: 12px;
+            font-weight: normal;
+        }
+    }
+    .home{
+        padding-top: 76px;
+    }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="welcome">
+      <div class="welcome__wrapper">
+        <div class="welcome__container">
+          <div class="welcome__row">
+            <h1>Добро пожаловать в Old Tried Partner!</h1>
+            <p>
+              У нас Вы можете приобрести автокосметику по низким ценам!
+              Наши товары довольно популярны в Краснодаре и Краснодарском крае.
+              Продукция Old Tried Partner подходит как для автомоек, так и для домашнего использования.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
+<style>
+
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
